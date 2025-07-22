dd4589ac8036fb5dae77075edd8aca96b1713606
Hajatiana <nirilantohajatiana032@gmail.com>
2025-07-23 00:09:45 +0300

    show some categories on  homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'current_menu' => 'home',
             'latest_products' => $productRepository->findBy([], ['createdAt' => 'DESC'], 4),
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'], 6),
         ]);
     }
 
